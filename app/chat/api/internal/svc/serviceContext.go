268c1fbcbd78867bc6f8649295cd6932491cb22c
离柒L <863577413@qq.com>
2023-03-27 10:01:23 +0800

    删除 api 里 serviceContext 的 model

--- app/chat/api/internal/svc/serviceContext.go
+++ app/chat/api/internal/svc/serviceContext.go
@@ -2,22 +2,14 @@ package svc
 
 import (
 	"giligili/app/chat/api/internal/config"
-	"giligili/app/chat/model"
-	"github.com/zeromicro/go-zero/core/stores/sqlx"
 )
 
 type ServiceContext struct {
 	Config config.Config
-
-	// model
-	ChatModel model.ChatModel
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
 		Config: c,
-
-		// model
-		ChatModel: model.NewChatModel(sqlx.NewMysql(c.MySQL.DataSource), c.Cache),
 	}
 }
--- app/video/api/internal/svc/serviceContext.go
+++ app/video/api/internal/svc/serviceContext.go
@@ -2,22 +2,14 @@ package svc
 
 import (
 	"giligili/app/video/api/internal/config"
-	"giligili/app/video/model"
-	"github.com/zeromicro/go-zero/core/stores/sqlx"
 )
 
 type ServiceContext struct {
 	Config config.Config
-
-	// model
-	VideoModel model.VideoModel
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
 		Config: c,
-
-		// model
-		VideoModel: model.NewVideoModel(sqlx.NewMysql(c.MySQL.DataSource), c.Cache),
 	}
 }
